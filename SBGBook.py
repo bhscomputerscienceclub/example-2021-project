4fecaf27f33199287b9606cf84d6d8b477e5b59c
Kai <kaizheng31@gmail.com>
2020-12-18 20:53:05 -0500

    finished sgbbook

--- SBGBook.py
+++ SBGBook.py
@@ -4,11 +4,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import requests
+import re
 
 driver = Chrome()
 driver.get("https://www.sbgbook.xyz/gbook/login/")
-username = ''
-password = ''
+username = '' #placeholder
+password = '' #placeholder
 driver.find_element_by_class_name("btn-success").click()
 time.sleep(1)
 driver.find_element_by_id('id_LogIn-username').send_keys(username)
@@ -21,6 +22,10 @@ driver.quit()
 s = requests.Session()
 for c in cookies:
     s.cookies[c['name']]= c['value']
-#still need to get sectionid, studentid, and collectionid from url
-r  = s.post('https://www.sbgbook.xyz/gbook/studentgradeframe/?section_id=796&student_id=5117&scollection_id=1013')
+
+title_search = re.search('student(.*)-(.*)-(.*)\/([0-9]*)', url, re.IGNORECASE)
+student_id,useless,collection_id,section_id = title_search.groups()
+
+
+r  = s.post('https://www.sbgbook.xyz/gbook/studentgradeframe/?section_id=' + section_id +'&student_id='+ student_id + '&scollection_id=' + collection_id)
 sbg_grades = r.content.decode()
