545b7dc60b502ed0edc51f92fc9790695d747c63
Kai <kaizheng31@gmail.com>
2020-12-23 19:06:05 -0500

    make it a method

--- InfiniteCampus.py
+++ InfiniteCampus.py
@@ -5,20 +5,22 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import requests
 
-username = ''
-password = ''
+def IC_grades():
+    username = ''
+    password = ''
 
-driver = Chrome()
-driver.get("https://lgca.infinitecampus.org/campus/portal/students/beachwood.jsp")
-driver.find_element_by_id('username').send_keys(username)
-driver.find_element_by_id('password').send_keys(password)
-driver.find_element_by_class_name('info').click()
-cookies=driver.get_cookies()
-driver.quit()
+    driver = Chrome()
+    driver.get("https://lgca.infinitecampus.org/campus/portal/students/beachwood.jsp")
+    driver.find_element_by_id('username').send_keys(username)
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_class_name('info').click()
+    cookies=driver.get_cookies()
+    driver.quit()
 
-s = requests.Session()
-for c in cookies:
-    s.cookies[c['name']]= c['value']
+    s = requests.Session()
+    for c in cookies:
+        s.cookies[c['name']]= c['value']
 
-r  = s.get('https://lgca.infinitecampus.org/campus/resources/portal/grades')
-ic_grades = r.content.decode()
+    r  = s.get('https://lgca.infinitecampus.org/campus/resources/portal/grades')
+    ic_grades = r.json()
+    return ic_grades[0]['terms']
--- SBGBook.py
+++ SBGBook.py
@@ -6,16 +6,30 @@ import time
 import requests
 import re
 
+#pls dont use this
+
 driver = Chrome()
 driver.get("https://www.sbgbook.xyz/gbook/login/")
-username = '' #placeholder
-password = '' #placeholder
+username = '' 
+password = '' 
 driver.find_element_by_class_name("btn-success").click()
 time.sleep(1)
 driver.find_element_by_id('id_LogIn-username').send_keys(username)
 driver.find_element_by_id('id_LogIn-password').send_keys(password)
 driver.find_element_by_id('lgnbtn').click()
 url = driver.current_url
+
+# courser = driver.find_element_by_class_name('dropdown-menu')
+
+# course_regex = re.search('student(.*)-(.*)">(.*)<\/a>', courser.get_attribute('innerHTML'), re.IGNORECASE)
+# a,b,course = course_regex.groups()
+# print(a,b,course)
+
+# grading_period = driver.find_elements_by_class_name('dropdown-menu')[1]
+# print(grading_period.get_attribute('innerHTML'))
+
+
+
 cookies=driver.get_cookies()
 driver.quit()
 
@@ -29,3 +43,4 @@ student_id,useless,collection_id,section_id = title_search.groups()
 
 r  = s.post('https://www.sbgbook.xyz/gbook/studentgradeframe/?section_id=' + section_id +'&student_id='+ student_id + '&scollection_id=' + collection_id)
 sbg_grades = r.content.decode()
+print(sbg_grades)
