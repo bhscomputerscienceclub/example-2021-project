d8d11e7a9bec3342d3935c7a0582cded64d301d1
Kai <kaizheng31@gmail.com>
2020-12-23 20:37:28 -0500

    a

--- InfiniteCampus.py
+++ InfiniteCampus.py
@@ -5,23 +5,26 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import requests
 
+
 def IC_grades():
-    username = ''
-    password = ''
+    username = ""
+    password = ""
 
     driver = Chrome()
     driver.get("https://lgca.infinitecampus.org/campus/portal/students/beachwood.jsp")
-    driver.find_element_by_id('username').send_keys(username)
-    driver.find_element_by_id('password').send_keys(password)
-    driver.find_element_by_class_name('info').click()
-    cookies=driver.get_cookies()
+    driver.find_element_by_id("username").send_keys(username)
+    driver.find_element_by_id("password").send_keys(password)
+    driver.find_element_by_class_name("info").click()
+    cookies = driver.get_cookies()
     driver.quit()
 
     s = requests.Session()
     for c in cookies:
-        s.cookies[c['name']]= c['value']
+        s.cookies[c["name"]] = c["value"]
 
-    r  = s.get('https://lgca.infinitecampus.org/campus/resources/portal/grades')
+    r = s.get("https://lgca.infinitecampus.org/campus/resources/portal/grades")
     ic_grades = r.json()
-    return ic_grades[0]['terms']
+    return ic_grades[0]["terms"]
+
+
 # print(IC_grades()[1]['courses'][0]['gradingTasks'][0]['progressPointsEarned'])
