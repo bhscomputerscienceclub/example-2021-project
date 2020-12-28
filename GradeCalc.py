7d7c78dc86c3d64855fd98195ba81612af1d0823
Kai <kaizheng31@gmail.com>
2020-12-28 16:29:52 -0500

    rename

--- GradeCalc.py
+++ GradeCalc.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from InfiniteCampus import IC_grades
-from thing import Data
+from data import Data
 import random
+
 app = Flask(__name__)
 
 userdata = {}
+
 
 @app.route("/", methods=["GET", "POST"])
 def function():
@@ -12,7 +14,7 @@ def function():
     ret = []
     Invalid_Number = False
     Invalid_User = False
-    
+
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -27,7 +29,7 @@ def function():
         except KeyError:
             Invalid_User = True
         else:
-            
+
             li = []
             for i in range(0, len(grades[0]["courses"])):
                 if (
@@ -49,24 +51,26 @@ def function():
                 ret[i].append(li[i].exam_percent_needed())
                 ret[i].append(li[i].q2_bonus_needed())
                 ret[i].append(li[i].q2_assignment_percent_needed(assignment_pts))
-            
-            num = random.randint(1,10000000000000000000000)
+
+            num = random.randint(1, 10000000000000000000000)
             while userdata.get(num, False):
-                num = random.randint(1,10000000000000000000000)
+                num = random.randint(1, 10000000000000000000000)
             userdata[num] = ret
-            return redirect(url_for('functionn', ret = num))
-    return render_template("index.html",Invalid_User = Invalid_User, Invalid_Number = Invalid_Number)
-
-
+            return redirect(url_for("functionn", ret=num))
+    return render_template(
+        "index.html", Invalid_User=Invalid_User, Invalid_Number=Invalid_Number
+    )
 
 
 @app.route("/results", methods=["GET", "POST"])
 def functionn():
     try:
-        num =int( request.args['ret'])
+        num = int(request.args["ret"])
         ret = userdata[num]
     except:
-        return redirect(url_for('function', Invalid_User = False, Invalid_Number = False))
-        
+        return redirect(url_for("function", Invalid_User=False, Invalid_Number=False))
+
     return render_template("results.html", len=len(ret), ret=ret)
+
+
 app.run(use_reloader=True, debug=True, port=5001)
