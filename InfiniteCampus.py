6df1381f3762328156cd19846b0d6f7bf378066d
Kai <kaizheng31@gmail.com>
2020-12-28 20:36:16 -0500

    Update InfiniteCampus.py

--- InfiniteCampus.py
+++ InfiniteCampus.py
@@ -1,15 +1,18 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import Firefox
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
 import time
 import requests
 import sys
 
+fireFoxOptions = Options()
+fireFoxOptions.set_headless()
 
 def IC_grades(username, password):
 
-    driver = Chrome()
+    driver = Firefox(options=fireFoxOptions)
     driver.get("https://lgca.infinitecampus.org/campus/portal/students/beachwood.jsp")
     driver.find_element_by_id("username").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
@@ -24,6 +27,5 @@ def IC_grades(username, password):
     r = s.get("https://lgca.infinitecampus.org/campus/resources/portal/grades")
     ic_grades = r.json()
     return ic_grades[0]["terms"]
-
 
 # print(IC_grades()[1]['courses'][0]['gradingTasks'][0]['progressPointsEarned'])
